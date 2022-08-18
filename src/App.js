ffcac15bcde0fe81cfef5022d20a32ae2167abcf
Uwaoma Cyruz <uwaomacyruz@gmail.com>
2022-08-18 13:12:01 -0700

    deleted test folder

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react"
 import "./app.css"
 import {movies} from "./movies"
 import Movie from "./movie"
-import {word} from "./tests/testing"
 function MovieList() {  
   return (
     <div className="movielist">
       {movies.map(movie =>{
        return <Movie key={movie.id} {...movie}/>
       })}
-      <p>{word}</p>
     </div>
    
   );
