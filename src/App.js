7d854657894ad9194b002d1bf0df787843e668e4
Uwaoma Cyruz <uwaomacyruz@gmail.com>
2022-08-18 17:40:30 -0700

    added useState

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React from "react"
 import "./app.css"
 import {movies} from "./movies"
 import Movie from "./movie"
+import {useState} from "react"
 function MovieList() {  
+  const[fruit, setfruit] = useState("apple")
+  const[number, setnumber] = useState(1)
+  const changefruit = () => {
+    if(fruit === "apple"){
+    setfruit("orange")}
+  else{setfruit("apple")}};
+  const changenumber = () =>{
+    setnumber(number + 1)
+    if(number === 5){
+      alert("stop");
+    }
+  }
   return (
+    <div>
     <div className="movielist">
       {movies.map(movie =>{
        return <Movie key={movie.id} {...movie}/>
-      })}
+      })}  
+     
     </div>
-   
+     <p className="para">{fruit}</p>
+      <button onClick = {changefruit} className="button">change fruits</button>
+      <p className="para">{number}</p>
+      <button onClick = {changenumber} className="button">change number</button>
+   </div>
   );
 }
 
